e0dc2a0f4c26cd7793a9aaf22e8045571cdaf604
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-03 23:43:55 +0300

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -8,7 +8,7 @@ batch_size = 64 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 3e-4
+learning_rate = 5e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 384
@@ -16,6 +16,8 @@ n_head = 6
 n_layer = 6
 dropout = 0.2
 # ------------
+
+
 
 torch.manual_seed(1337)
 
