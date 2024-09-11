d48a89c835057353b32e3899aba07816457ebb6d
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-11 23:13:11 +0300

    Update bigram.py

--- bigram.py
+++ bigram.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-
+ 
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
